c4d599cc7b7a21b384d6a938afdb697b9631731e
maria <maria@DESKTOP-9E9VDQS>
2022-03-13 22:59:14 +0100

    Mensaje consola

---
+++ TestProject/src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB.");
+		
+		sc.close();
+	}
+	
+}
